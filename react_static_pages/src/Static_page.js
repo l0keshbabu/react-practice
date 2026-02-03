48df703187c02ca0c41af21a51baaf5e1171dda3
l00kesh <kattalokeshbabuk@gmail.com>
2026-02-03 14:30:58 +0530

    Initialize React practice workspace with multiple projects

---
+++ react_static_pages/src/Static_page.js
@@ -0,0 +1,39 @@
+import logo from "./assets/images/r_logo.png";
+import "./Static_page.css"
+function Header(){
+    return(
+        <header>
+        <nav>
+        <a href="https://react.dev/"><img className="logo" src={logo} alt="react_logo"></img></a>
+        <span>ReactFacts</span>
+        </nav>
+        </header>
+        )
+}        
+function MainContent(){
+    return (
+        <main>
+        <h1>Fun facts about React!</h1>
+        <ul className="facts-list">
+            <li>Was first release in 2013</li>
+            <li>Was originally created by Jordan Walke</li>
+            <li>Has well over 200K stars on GitHub</li>
+            <li>Is maintained by Meta</li>
+            <li>Powers thousands of enterprise apps, including mobile apps</li>
+        </ul>
+        </main>
+    )
+} 
+    
+
+function Static(){
+    return(
+        <>
+    <Header/>
+    <MainContent/>
+    </>
+    )
+    
+}
+           
+export default Static;
