0f542c5c978204561427e9dc50ace1dcab14cf0e
l00kesh <kattalokeshbabuk@gmail.com>
2026-02-03 15:12:35 +0530

    Organize Scrimba practice modules within single React app

---
+++ react-practice/src/App.js
@@ -0,0 +1,11 @@
+import Static from "./react-static-page/Static_page";
+
+function App() {
+  return (
+    <>
+      <Static />
+    </>
+  );
+}
+
+export default App;
--- react-practice/src/index.js
+++ react-practice/src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Static from './Static_page';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Static />
+    <App />
   </React.StrictMode>
 );
 
