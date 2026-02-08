4cd77cf7eb8df4775dbd6d6c667b8105114a714e
l00kesh <kattalokeshbabuk@gmail.com>
2026-02-08 11:39:01 +0530

    Add multiple travel entries using reusable component

--- react-practice/src/App.js
+++ react-practice/src/App.js
@@ -1,10 +1,12 @@
 // import Static from "./react-static-page/Static_page";
 import Traveljournal from "./travel-journal/travel";
 
+
 function App() {
   return (
     <>
-      <Traveljournal />
+      <Traveljournal
+      />
     </>
   );
 }
--- react-practice/src/travel-journal/travel.js
+++ react-practice/src/travel-journal/travel.js
@@ -1,6 +1,8 @@
 import globe from "../assets/images/travel-journal/Globe.png"
-import shiridi from "../assets/images/travel-journal/shiridi.png"
 import marker from "../assets/images/travel-journal/marker.png"
+import shiridi from "../assets/images/travel-journal/shiridi.png"
+import golconda from "../assets/images/travel-journal/golconda.png"
+import konda from "../assets/images/travel-journal/kondapalli.png"
 import "./travel.css"
 
 // -------------------------travel journal components start------------------------
@@ -14,27 +16,21 @@ function Header(){
     )
 }
 
-function Entry(){
+function Entry(props){
     return (
         <div className="travel-entry">
             <div className="main-image-container">
-            <img src={shiridi} alt="shiridi" className="main-image"></img>
+            <img src={props.image.src} alt={props.image.alt} className="main-image"></img>
             </div>
             <div className="info-container">
                 <img src={marker} alt="location pin" className="marker"></img>
-                <span className="country">India</span>
-                <a href="https://www.google.com/maps/place/Shri+Sai+Baba+Samadhi+Mandir/@19.7
-                65022,74.4665727,15.62z/data=!4m15!1m8!3m7!1s0x3bdc5ba7461768af:0xd802d2c2943c9
-                9c!2sShirdi,+Maharashtra!3b1!8m2!3d19.7645364!4d74.4762124!16zL20vMDI5djcw!3m5!
-                1s0x3bdc5b95bf70c2f7:0x23dbc9b2db45374!8m2!3d19.7661782!4d74.4769973!16s%2Fg%2F11
-                g01_82b2?entry=ttu&g_ep=EgoyMDI2MDIwMy4wIKXMDSoASAFQAw%3D%3D">View on Google Maps
+                <span className="country">{props.country}</span>
+                <a href={props.googlemapslink}>View on Google Maps
 
                 </a>
-                <h2 className="entry-title">Shirdi</h2>
-                <p className="trip-dates">12 Jan, 2019 - 15 Jan, 2019</p>
-                <p className="entry-text">Shirdi is a town and pilgrimage site in the Indian state of Maharashtra.
-                     Shirdi is located in the Rahata taluka of Ahmednagar district. It is most 
-                     well known for being the home of the 19th-century Indian saint Sai Baba.</p>
+                <h2 className="entry-title">{props.title}</h2>
+                <p className="trip-dates">{props.dates}</p>
+                <p className="entry-text">{props.text}</p>
             </div>
         </div>
     )
@@ -45,8 +41,69 @@ export default function Traveljournal(){
     return(
         <>
         <Header/>
-        <Entry/>
+        <Entry image={{src:shiridi,
+             alt:"shirdi"}} 
+
+             title="Shirdi"
+
+             country="India"
+
+             googlemapslink="https://www.google.com/maps/place/Shri+Sai+Baba+Samadhi+Mandir/@19.765022,74.4665727,15.62z/data=!4m15!1m8!3m7!1s0x3bdc5ba7461768af:0xd802d2c2943c99c!2sShirdi,+Maharashtra!3b1!8m2!3d19.7645364!4d74.4762124!16zL20vMDI5djcw!3m5!1s0x3bdc5b95bf70c2f7:0x23dbc9b2db45374!8m2!3d19.7661782!4d74.4769973!16s%2Fg%2F11g01_82b2?entry=ttu&g_ep=EgoyMDI2MDIwMy4wIKXMDSoASAFQAw%3D%3D"
+
+            dates="12 Jan, 2019 - 15 Jan, 2019"
+            
+            text="Shirdi is a town and pilgrimage site in the Indian state of Maharashtra.
+                     Shirdi is located in the Rahata taluka of Ahmednagar district. It is most 
+                     well known for being the home of the 19th-century Indian saint Sai Baba." />
+
+
+            <Entry image={{src:konda,
+             alt:"Kondapalli Fort"}} 
+
+             title="Kondapalli Fort"
+
+             country="India"
+
+             googlemapslink="https://www.google.com/maps/place/Kondapalli+Fort/@16.6128589,80.508296,13.56z/data=!4m6!3m5!1s0x3a35ebc28527a6ff:0x33df4b63d661a919!8m2!3d16.6254258!4d80.5306487!16s%2Fm%2F09gc7fq?entry=ttu&g_ep=EgoyMDI2MDIwNC4wIKXMDSoASAFQAw%3D%3D"
+
+            dates="21 May, 2023 - 22 May, 2023"
+            
+            text="Kondapalli Fort, also locally known as Kondapalli Kota and Kondapalli Quilla,
+             lies to the west of Kondapalli in Ibrahimpatnam mandal of NTR district in Andhra
+              Pradesh, India was built by Prolaya Vema Reddy of Reddi Kingdom during
+               the 14th century CE. It was initially built as a leisure place and business center
+                and later served as a military training base for the British rulers. According to
+                 some other historians it was built in 1360 CE by Anna Vema Reddy after he 
+                 captured Kondapalli from Mudigonda Chalukyas, the fort has been home to several 
+                 dynasties, from the Reddi rulers Gajapati dynasty to the Nizam Nawabs,
+                 and then the East India Company.
+                 " />            
+
+            <Entry image={{src:golconda,
+             alt:"Golconda Fort"}} 
+
+             title="Golconda Fort"
+
+             country="India"
+
+             googlemapslink="https://www.google.com/maps/place/Golconda+Fort/@17.3833091,78.3984773,17z/data=!3m1!4b1!4m6!3m5!1s0x3bcb968ea6aea98f:0x6b76ecdfd65a335a!8m2!3d17.3833091!4d78.4010522!16zL20vMDdtd2p5?entry=ttu&g_ep=EgoyMDI2MDIwNC4wIKXMDSoASAFQAw%3D%3D"
+
+            dates="14 July, 2025 - 15 July, 2025"
+            
+            text="Golconda is a fortified citadel and ruined city located on the western
+             outskirts of Hyderabad, Telangana, India.[1][2] The fort was originally built 
+             by Kakatiya ruler Pratāparudra in the 11th century out of mud walls.[3] It was
+              ceded to the Bahmani Kings from Musunuri Nayakas during the reign of Bahmani
+               Sultan Mohammed Shah I, during the first Bahmani-Vijayanagar War. Following 
+               the death of Sultan Mahmood Shah, the Sultanate disintegrated and Sultan Quli,
+                who had been appointed as the Governor of Hyderabad by the Bahmani Kings,
+                 fortified the city and made it the capital of the Golconda Sultanate. 
+                 Because of the vicinity of diamond mines, especially Kollur Mine, Golconda 
+                 flourished as a trade centre of large diamonds known as Golconda Diamonds.
+                 " />         
         </>
+
+        
 
     )
 }
