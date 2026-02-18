4f71bd81b26c977072bc99a4e507fcee8b143317
l00kesh <kattalokeshbabuk@gmail.com>
2026-02-18 14:46:02 +0530

    Add styling for Meme Generator main component

--- react-practice/src/meme-generator/Main.js
+++ react-practice/src/meme-generator/Main.js
@@ -1,20 +1,21 @@
 import meme1 from "../assets/images/Meme/meme1.png"
+import "./Memes.css"
 export default function Main(){
     return(
         <main>
             <div className="form">
                 <label>Top Text
-                    <input type="text" placeholder="One does not simply" name="topText"></input>
+                    <input type="text" placeholder="Shut Up" name="topText"></input>
                 </label>
                 <label>Bottom Text
-                    <input type="text" placeholder="Walk into Morder" name="BottomText"></input>
+                    <input type="text" placeholder="and take my Money" name="BottomText"></input>
                 </label>
                 <button>Get a new meme image 🖼</button>
             </div>
             <div className="meme">
                 <img src={meme1} alt="memeImage" />
-                <span className="top">One does not simply</span>
-                <span className="bottom">Walk into Mordor</span>
+                <span className="top">Shut Up</span>
+                <span className="bottom">and take my money</span>
             </div>
         </main>
     ) 
