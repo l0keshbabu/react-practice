6b0b0bfd257cc9192f504c2b44504a5334c1cacd
l00kesh <kattalokeshbabuk@gmail.com>
2026-02-26 14:51:40 +0530

    Add reusable Die component and static dice layout

--- react-practice/src/TenziesApp/App.js
+++ react-practice/src/TenziesApp/App.js
@@ -1,8 +1,20 @@
 import "./tenxies.css"
+import Die from "./Die"
 export default function App(){
     return(
         <main>
-
+            <div className="dice-container">
+                <Die num={1}/>
+                <Die num={1}/>
+                <Die num={1}/>
+                <Die num={1}/>
+                <Die num={1}/>
+                <Die num={1}/>
+                <Die num={1}/>
+                <Die num={1}/>
+                <Die num={1}/>
+                <Die num={1}/>
+            </div>
         </main>
     )
 }
