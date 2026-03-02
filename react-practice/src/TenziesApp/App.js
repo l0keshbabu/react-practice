dd277ba28f017c32542f4887303f275263399026
l00kesh <kattalokeshbabuk@gmail.com>
2026-03-02 14:37:50 +0530

    Convert dice array to object structure with unique IDs

--- react-practice/src/TenziesApp/App.js
+++ react-practice/src/TenziesApp/App.js
@@ -1,21 +1,22 @@
 import "./tenxies.css"
 import Die from "./Die"
 import { useState } from "react"
+import { nanoid } from "nanoid"
 
 
 
-export default function App(){
+export default function App(){ 
 
     function generateAllNewDice() {
         return new Array(10)
             .fill(0)
-            .map(() => Math.ceil(Math.random() * 6))
+            .map(() => ({value:Math.ceil(Math.random() * 6),isHeld:false,id:nanoid()}))
     }
     function rollDice(){
         setDice(generateAllNewDice)
     }
     const [dice,setDice] = useState(generateAllNewDice())
-    const diceEle = dice.map(num => <Die value={num}/>)
+    const diceEle = dice.map(DieObj => <Die key={DieObj.id} value={DieObj.value}/>)
     return(
         <main>
             <div className="dice-container">
