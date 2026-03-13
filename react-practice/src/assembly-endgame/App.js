b2a54152df6ef029e0e64cc628d319a7270eb302
l00kesh <kattalokeshbabuk@gmail.com>
2026-03-13 16:15:22 +0530

    add game status section and render language chips

--- react-practice/src/assembly-endgame/App.js
+++ react-practice/src/assembly-endgame/App.js
@@ -1,12 +1,32 @@
 import "./endgame.css"
+import { languages } from "./languages"
 
 
 
 export default function App(){
+
+    const languageElements = languages.map(lang => {
+        const styles={
+            backgroundColor:lang.backgroundColor,
+            color:lang.color
+        }
+        return (
+            <span key={lang.name} className="chip" style={styles}>{lang.name}</span>
+        )
+    })
     return (
-        <header>
-            <h1>Assembly: Endgame</h1>
-                <p>Guess the word within 8 attempts to keep the programming world safe from Assembly!</p>
-        </header>
+        <main>
+            <header>
+                <h1>Assembly: Endgame</h1>
+                    <p>Guess the word within 8 attempts to keep the programming world safe from Assembly!</p>
+            </header>
+            <section className="game-status">
+                 <h2>You win!</h2>
+                <p>Well done! 🎉</p>
+            </section>
+            <section className="language-chips">
+                {languageElements}
+            </section>
+        </main>
     )
 }
